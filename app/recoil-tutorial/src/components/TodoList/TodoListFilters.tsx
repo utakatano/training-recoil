cacbc6739a603ad7d66d9aef92b9060da1705a9d
Kei Yagi <keiyagi@Keis-MacBook-Air.local>
2021-05-20 23:51:24 +0900

    use-selector-filter-func

---
+++ app/recoil-tutorial/src/components/TodoList/TodoListFilters.tsx
@@ -0,0 +1,40 @@
+import React from 'react'
+import { useRecoilState } from 'recoil'
+import { TodoFilterProps, todoListFilterState } from '../../atoms/todoList'
+
+interface FilterMenu {
+  value: TodoFilterProps
+  label: string
+}
+
+const todoFilterMenu: ReadonlyArray<FilterMenu> = [
+  {
+    value: 'Show All',
+    label: 'All'
+  }, {
+    value: 'Show Completed',
+    label: 'Completed'
+  }, {
+    value: 'Show Uncompleted',
+    label: 'Uncompleted'
+  }
+]
+
+export const TodoListFilters: React.FC = () => {
+  const [filter, setFilter] = useRecoilState(todoListFilterState)
+
+  const updateFilter = ({target: {value}}: React.ChangeEvent<HTMLSelectElement>) => {
+    setFilter(value as TodoFilterProps)
+  }
+
+  return (
+    <div>
+      Filter:
+      <select value={filter} onChange={updateFilter}>
+        {todoFilterMenu.map(({value, label}, index) => (
+          <option key={`menu-${index}`} value={value}>{label}</option>
+        ))}
+      </select>
+    </div>
+  )
+}
---
+++ app/recoil-tutorial/src/selectors/todoList.ts
@@ -0,0 +1,21 @@
+import { selector } from 'recoil'
+import { todoListFilterState, todoListState } from '../atoms/todoList'
+
+export const filteredTodoListState = selector({
+  key: 'filteredTodoListState',
+  get: ({ get }) => {
+    const filter = get(todoListFilterState)
+    const list = get(todoListState)
+
+    switch(filter) {
+      case 'Show Completed':
+        return list.filter((item) => item.isComplete)
+
+      case 'Show Uncompleted':
+        return list.filter((item) => !item.isComplete)
+      
+      default:
+        return list
+    }
+  }
+})
